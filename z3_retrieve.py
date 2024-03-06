a51b7cdab8c26d79410b986ab87f7b51ee62eeee
Grant Clary <gclary9@yahoo.com>
2024-03-06 09:23:25 -0600

    updated format in retrieve file make it more readable

--- z3_retrieve.py
+++ z3_retrieve.py
@@ -6,15 +6,15 @@ cr = conn.cursor()
 #pulls the list of all the tables in that databse
 cr.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables = cr.fetchall()
-print (tables)
+print ("list of tables in the DB file:\n",tables,"\n")
 
 #i found out the table name is movies
 #this will pull all the columns from that table
 cr.execute("PRAGMA table_info({});".format("movies"))
 columns = cr.fetchall()
 column_names = [column[1] for column in columns]
-print (column_names)
+print ("list of columns in the movie table:\n",column_names,"\n")
 
 #pulls data from table "movies"
 cr.execute("SELECT * FROM movies")
-print(cr.fetchall())
+print("list of data in the movies table:\n",cr.fetchall(),"\n")
