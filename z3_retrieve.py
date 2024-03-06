43439f934c4b9b4d1c07fd358ec035fd3acee75a
Grant Clary <gclary9@yahoo.com>
2024-03-06 09:47:01 -0600

    update retrieve file with pulling specific data from DB

--- z3_retrieve.py
+++ z3_retrieve.py
@@ -18,3 +18,8 @@ print ("list of columns in the movie table:\n",column_names,"\n")
 #pulls data from table "movies"
 cr.execute("SELECT * FROM movies")
 print("list of data in the movies table:\n",cr.fetchall(),"\n")
+
+#pulls specific data
+cr.execute("SELECT title, director, year FROM movies WHERE year = ?;", (2010,))
+moviesFrom2010 = cr.fetchall()
+print ("movies from 2010:\n",moviesFrom2010)
