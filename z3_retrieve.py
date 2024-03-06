31c075a6ac5da78e5ce41b0c426132c6ceac528a
Grant Clary <gclary9@yahoo.com>
2024-03-06 09:20:06 -0600

    update retrieve file. pulls column names from a table

--- z3_retrieve.py
+++ z3_retrieve.py
@@ -8,6 +8,13 @@ cr.execute("SELECT name FROM sqlite_master WHERE type='table';")
 tables = cr.fetchall()
 print (tables)
 
+#i found out the table name is movies
+#this will pull all the columns from that table
+cr.execute("PRAGMA table_info({});".format("movies"))
+columns = cr.fetchall()
+column_names = [column[1] for column in columns]
+print (column_names)
+
 #pulls data from table "movies"
 cr.execute("SELECT * FROM movies")
 print(cr.fetchall())
